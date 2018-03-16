8259cb757f05251a9fa150932e83c75b5bad46f5
Sam Haycraft <shaycraft@metlife.com>
2018-03-16 15:12:26 -0600

    List item for moves list

--- src/index.js
+++ src/index.js
@@ -84,6 +84,18 @@ class Game extends React.Component {
     const current = history[history.length - 1];
     const winner = calculateWinner(current.squares);
 
+    const moves = history.map((step, move) => {
+      console.log(move);
+      const desc = move ?
+        'Go to move #' + move :
+        'Go to game start';
+      return (
+        <li>
+          <button onClick={() => this.jumpTo(move)}>{desc}</button>
+        </li>
+      );
+    })
+
     let status;
     if (winner) {
       status = 'Winner: ' + winner;
@@ -94,14 +106,14 @@ class Game extends React.Component {
     return (
       <div className="game">
         <div className="game-board">
-          <Board 
+          <Board
             squares={current.squares}
             onClick={(i) => this.handleClick(i)}
           />
         </div>
         <div className="game-info">
           <div>{status}</div>
-          <ol>{/* TODO */}</ol>
+          <ol>{moves}</ol>
         </div>
       </div>
     );
